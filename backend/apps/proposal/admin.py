f2f7d141344d0272c503830511d003b5e3de53c9
christianalcantara <christian.douglas.alcantara@gmail.com>
2023-08-30 06:00:32 -0300

    admin

--- backend/apps/proposal/admin.py
+++ backend/apps/proposal/admin.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 
+from .choices import LoanStatus
 from .models import Field, LoanApplication, Proposal, Value
 
 
@@ -20,38 +22,58 @@ class ValueAdminInLine(admin.TabularInline):
 
 @admin.register(Proposal)
 class ProposalAdmin(admin.ModelAdmin):
-    verbose_name_plural = "1. Toppings"
     inlines = (FieldAdminInLine,)
     list_display = ("name",)
 
 
 @admin.register(LoanApplication)
 class LoanApplicationAdmin(admin.ModelAdmin):
+    change_form_template = "admin/proposal/change_form.html"
+
     inlines = (ValueAdminInLine,)
     list_display = (
-        "id",
+        "partner_name",
+        "partner_document",
         "proposal",
         "creation_date",
-        "approved",
+        "status",
     )
-    list_display_links = (
-        "id",
-        "proposal",
-    )
-    list_filter = ("proposal", "approved")
+    list_filter = ("proposal", "status")
+    search_fields = ("partner_name", "partner_document")
     readonly_fields = (
+        "partner_name",
+        "partner_document",
         "proposal",
-        "approved",
+        "status",
     )
 
+    def has_add_permission(self, request, obj=None):
+        return False
 
-@admin.register(Field)
-class FieldAdmin(admin.ModelAdmin):
-    list_display = ("label", "name", "field_type", "proposal")
-    list_filter = ("proposal", "field_type")
+    def response_change(self, request, obj):
+        if "_approve" in request.POST:
+            obj.status = LoanStatus.APPROVED
+            obj.save()
+            self.message_user(request, f"{obj} Approved!")
+            return HttpResponseRedirect(".")
+        if "_negate" in request.POST:
+            obj.status = LoanStatus.NOT_APPROVED
+            obj.save()
+            self.message_user(request, f"{obj} Not Approved!", level=messages.WARNING)
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
 
 
-@admin.register(Value)
-class ValueAdmin(admin.ModelAdmin):
-    list_display = ("loan_application", "field", "value")
-    list_filter = ("loan_application",)
+# @admin.register(Field)
+# class FieldAdmin(admin.ModelAdmin):
+#     list_display = ("label", "name", "field_type", "proposal")
+#     list_filter = ("proposal", "field_type")
+#
+#
+# @admin.register(Value)
+# class ValueAdmin(admin.ModelAdmin):
+#     list_display = ("loan_application", "field", "value")
+#     list_filter = ("loan_application",)
+#
+#     def has_add_permission(self, request, obj=None):
+#         return False
