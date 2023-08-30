43fedfaf4ba9af179a9f602c922bd4974cbe89e2
christianalcantara <christian.douglas.alcantara@gmail.com>
2023-08-30 06:00:10 -0300

    loan processor api

--- backend/apps/core/loan_processor.py
+++ backend/apps/core/loan_processor.py
@@ -3,18 +3,13 @@ import requests
 
 def evaluate_loan(document: str, name: str):
     url = "https://loan-processor.digitalsys.com.br/api/v1/loan/"
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "document": document,
-        "name": name
-    }
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+    data = {"document": document, "name": name}
     response = requests.post(url=url, data=data, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"[{response.status_code}] {response.text}")
     return response.json()
 
 
 if __name__ == "__main__":
     r = evaluate_loan("88815935134", "Christian Douglas")
-    print(">>>", r)
